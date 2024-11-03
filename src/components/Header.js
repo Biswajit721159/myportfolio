7b7481d46f5dc212975aca237259978577a4668a
Biswajit721159 <bg5050525@gmail.com>
2024-11-03 22:10:02 +0530

    adding more

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header className="bg-blue-600 text-white p-3 shadow-md">
             <div className="container mx-auto flex justify-between items-center">
                 <h1 onClick={() => history('/')} className="text-xl font-bold cursor-pointer">MyPortfolio</h1>
-                <nav className="space-x-6">
+                <nav className="space-x-6 flex flex-wrap">
                     <a href="#about" className="text-white hover:text-blue-200 transition duration-200">About</a>
                     <a href="#workExperience" className="text-white hover:text-blue-200 transition duration-200">Work Experience</a>
                     <a href="#projects" className="text-white hover:text-blue-200 transition duration-200">Projects</a>
